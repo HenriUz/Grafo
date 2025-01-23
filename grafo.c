3e74a13362178bb21f8d8f94801a3e6ba1fcbdd1
HenriUz <d2023001316@unifei.edu.br>
2025-01-23 11:33:07 -0300

    Finished inicials structs and 'cria_grafo' function.

--- grafo.c
+++ grafo.c
@@ -1,1 +1,31 @@
+#include <stdlib.h>
 #include "grafo.h"
+
+struct Grafo {
+    int nVertices; // Quantidade de vértices.
+    Vertice *v; // Vértices do grafo.
+};
+
+struct Vertice {
+    int id; // ID do vértice.
+    int nAresta; // Quantidade de arestas.
+    Aresta *a; // Arestas do vértice.
+    Vertice *prox; // Próximo vértice na lista.
+};
+
+struct Aresta {
+    int destino; // ID do vértice de destino.
+    float tamanho; // Tamanho da aresta.
+};
+
+Grafo *cria_grafo() {
+    Grafo *g = (Grafo *)malloc(sizeof(Grafo));
+    // Verificando se o chunk foi alocado corretamente.
+    if (!g) {
+        return NULL;
+    }
+    // Inicializando valores iniciais.
+    g->nVertices = 0;
+    g->v = NULL;
+    return g;
+}
--- grafo.h
+++ grafo.h
@@ -1,4 +1,15 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
+typedef struct Grafo Grafo;
+typedef struct Vertice Vertice;
+typedef struct Aresta Aresta;
+
+/*
+Descrição: função responsável por criar e inicializar uma estrutura Grafo.
+Entrada: nada.
+Saída: ponteiro para a estrutura criada.
+*/
+Grafo *cria_grafo();
+
 #endif //GRAFO_H
