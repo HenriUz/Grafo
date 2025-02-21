ca68422107c5a62e2e5aaa7109be79d3520fc67f
HenriUz <d2023001316@unifei.edu.br>
2025-02-21 09:30:08 -0300

    Changed aresta struct, changed loops in 'verifica' functions, and fixed 'grau' functions.

--- grafo.c
+++ grafo.c
@@ -15,9 +15,9 @@ struct Vertice {
 
 struct Aresta {
     int id; // ID da aresta, útil para identificar arestas paralelas.
-    int destino; // ID do vértice de destino.
     float tamanho; // Tamanho da aresta.
     Aresta *prox; // Próxima aresta na lista.
+    Vertice *destino; // Vértice de destino.
 };
 
 Grafo *cria_grafo() {
@@ -38,14 +38,12 @@ Entrada: ponteiro para o grafo, inteiro do id do vértice.
 Saída: ponteiro para o vértice, e nulo caso não exista.
 */
 Vertice *verifica_vertice(const Grafo *g, const int id) {
-    Vertice *v = g->v;
     // Verificando se o vértice já existe.
-    while (v) {
+    for (Vertice *v = g->v; v; v = v->prox) {
         if (v->id == id) {
             // Vértice existe.
             return v;
         }
-        v = v->prox;
     }
     // Vértice não existe.
     return NULL;
@@ -60,12 +58,12 @@ int adiciona_vertice(Grafo *g, const int id) {
         }
         // Inicializando valores.
         v->id = id;
-        v->prox = g->v;
         v->nAresta = 0;
         v->a = NULL;
+        v->prox = g->v;
         // Inserindo no início para a complexidade ser O(1).
-        g->v = v;
         g->nVertices++;
+        g->v = v;
         return 1;
     }
     return 0;
@@ -77,22 +75,31 @@ Entrada: ponteiro para o vértice, inteiro do id da aresta.
 Saída: ponteiro para a aresta, e nulo caso não exista.
 */
 Aresta *verifica_aresta(const Vertice *v, const int id) {
-    Aresta *a = v->a;
     // Verificando se a aresta já existe.
-    while (a) {
+    for (Aresta *a = v->a; a; a = a->prox) {
         if (a->id == id) {
             // Aresta existe.
             return a;
         }
-        a = a->prox;
     }
     // Aresta não existe.
     return NULL;
 }
 
-int adiciona_aresta(const Grafo *g, const int idVertice, const int idAresta, const int idDestino, const float tamanho) {
-    Vertice *v = verifica_vertice(g, idVertice);
-    if (v && !verifica_aresta(v, idAresta)) {
+int adiciona_aresta(const Grafo *g, const int idOrigem, const int idDestino, const int idAresta, const float tamanho) {
+    Vertice *origem = NULL, *destino = NULL;
+    // Buscando os vértices (sem usar a verifica_vertice para buscar os dois em um único loop).
+    for (Vertice *v = g->v; v; v = v->prox) {
+        // 2 ifs para identificar laço.
+        if (v->id == idOrigem) {
+            origem = v;
+        }
+        if (v->id == idDestino) {
+            destino = v;
+        }
+    }
+    // Verificando se a aresta pode ser inserida (vértices existem, e a aresta ainda não existe).
+    if (origem && destino && !verifica_aresta(origem, idAresta)) {
         // Criando uma nova aresta.
         Aresta *a = (Aresta *)malloc(sizeof(Aresta));
         if (!a) {
@@ -100,39 +107,44 @@ int adiciona_aresta(const Grafo *g, const int idVertice, const int idAresta, con
         }
         // Inicializando valores.
         a->id = idAresta;
-        a->destino = idDestino;
         a->tamanho = tamanho;
-        a->prox = v->a;
+        a->prox = origem->a;
+        a->destino = destino;
         // Inserindo no início para complexidade ser O(1).
-        v->a = a;
-        v->nAresta++;
+        origem->nAresta++;
+        origem->a = a;
         return 1;
     }
     return 0;
 }
 
 int grau_de_entrada(const Grafo *g, const int id) {
-    int grau = -1;
-    const Vertice *v = g->v;
-    // Percorrendo os vértices.
-    while (v) {
-        if (v->id != id) {
+    int grau = 0, aux = -1;
+    if (g) {
+        // Percorrendo os vértices.
+        for (const Vertice *v = g->v; v; v = v->prox) {
+            // Verificando se o vértice existe.
+            if (v->id == id) {
+                aux = 1;
+            }
             // Somando o grau caso a aresta tenha como destino o vértice informado no id.
             for (const Aresta *a = v->a; a; a = a->prox) {
-                grau += a->destino == id ? 1 : 0;
+                grau += a->destino->id == id ? 1 : 0;
             }
         }
-        v = v->prox;
     }
     // Retornando o grau.
-    return grau == -1 ? grau : grau + 1;
+    return aux == -1 ? aux : grau;
 }
 
 int grau_de_saida(const Grafo *g, const int id) {
-    const Vertice *v = g->v;
-    // Buscando o vértice correspondente.
-    while (v && v->id != id) {
-        v = v->prox;
+    const Vertice *v = NULL;
+    if (g) {
+        v = g->v;
+        // Buscando o vértice correspondente.
+        while (v && v->id != id) {
+            v = v->prox;
+        }
     }
     // Retornando o grau.
     return !v ? -1 : v->nAresta;
--- grafo.h
+++ grafo.h
@@ -6,37 +6,37 @@ typedef struct Vertice Vertice;
 typedef struct Aresta Aresta;
 
 /*
-Descrição: função responsável por criar e inicializar uma estrutura Grafo.
+Descrição: função responsável por criar e inicializar um grafo.
 Entrada: nada.
-Saída: ponteiro para a estrutura criada.
+Saída: ponteiro para o grafo, e nulo caso um erro tenha ocorrido.
 */
 Grafo *cria_grafo();
 
 /*
 Descrição: função responsável por criar e adicionar um vértice ao grafo.
 Entrada: ponteiro para o grafo, inteiro do id do vértice.
-Saída: 1 - sucesso, 0 - vértice já existente ou grafo não existente, −1 - erro na alocação de memória.
+Saída: 1 - sucesso, 0 - vértice já existe ou grafo não existe, −1 - erro na alocação de memória.
 */
 int adiciona_vertice(Grafo *g, int id);
 
 /*
 Descrição: função responsável por criar e adicionar uma aresta ao vértice.
-Entrada: ponteiro para o grafo, inteiro do id do vértice de origem, inteiro do id da aresta, inteiro do id do vértice de destino, ponto flutuante do tamanho da aresta.
-Saída: 1 - sucesso, 0 - vértice ou grafo não existente, ou a aresta já existe, −1 - erro na alocação de memória.
+Entrada: ponteiro para o grafo, inteiro do id do vértice de origem, inteiro do id do vértice de destino, inteiro do id da aresta, ponto flutuante do tamanho da aresta.
+Saída: 1 - sucesso, 0 - vértices ou grafo não existe, ou a aresta já existe, −1 - erro na alocação de memória.
 */
-int adiciona_aresta(const Grafo *g, int idVertice, int idAresta, int idDestino, float tamanho);
+int adiciona_aresta(const Grafo *g, int idOrigem, int idDestino, int idAresta, float tamanho);
 
 /*
 Descrição: função responsável por retornar o grau de entrada de um vértice.
 Entrada: ponteiro para o grafo, inteiro do id do vértice.
-Saída: grau de entrada, −1 - vértice não existente.
+Saída: grau de entrada, −1 - vértice ou grafo não existe.
 */
 int grau_de_entrada(const Grafo *g, int id);
 
 /*
 Descrição: função responsável por retornar o grau de saída de um vértice.
 Entrada: ponteiro para o grafo, inteiro do id do vértice.
-Saída: grau de saída, −1 - vértice não existente.
+Saída: grau de saída, −1 - vértice ou grafo não existe.
 */
 int grau_de_saida(const Grafo *g, int id);
 
